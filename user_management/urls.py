5ba0e11898f7683cf7e28643e939a72bd091a1cf
GeorgeElkomos <gelkomos@lightidea.org>
2025-05-26 07:21:22 +0300

    Update urls.py

--- user_management/urls.py
+++ user_management/urls.py
@@ -5,31 +5,56 @@ app_name = 'user_management'
 
 urlpatterns = [
     # Authentication endpoints
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('token-expired/', TokenExpiredView.as_view(), name='token-expired'),
-    
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("token-expired/", TokenExpiredView.as_view(), name="token-expired"),
     # User management endpoints
-    path('users/', ListUsersView.as_view(), name='list-users'),
-    path('users/permission/<int:user_id>/', UpdateUserPermissionView.as_view(), name='update_user_permission'),
-    path('users/update/', UserUpdateView.as_view(), name='user_update'),
-    path('users/delete/', UserDeleteView.as_view(), name='user_delete'),
-
+    path("users/", ListUsersView.as_view(), name="list-users"),
+    path(
+        "users/permission/<int:user_id>/",
+        UpdateUserPermissionView.as_view(),
+        name="update_user_permission",
+    ),
+    path("users/update/", UserUpdateView.as_view(), name="user_update"),
+    path("users/delete/", UserDeleteView.as_view(), name="user_delete"),
+    path("users/level/update", UpdateUserLevelView.as_view(), name="user_delete"),
 
 
     # User level management endpoints
-    path('levels/', UserLevelListView.as_view(), name='user-level-list'),
-    path('levels/create/', UserLevelCreateView.as_view(), name='user-level-create'),
-    path('levels/update/', UserLevelUpdateView.as_view(), name='level_update'),
-    path('levels/delete/', UserLevelDeleteView.as_view(), name='level_delete'),
-    path("users/level/update", UpdateUserLevelView.as_view(), name="user_delete"),
+    path("levels/", UserLevelListView.as_view(), name="user-level-list"),
+    path("levels/create/", UserLevelCreateView.as_view(), name="user-level-create"),
+    path("levels/update/", UserLevelUpdateView.as_view(), name="level_update"),
+    path("levels/delete/", UserLevelDeleteView.as_view(), name="level_delete"),
+
 
     # Notification management endpoints
     #
-    path('Notifications/unread', UnRead_Notification.as_view(), name='unread-notifications'),
-    path('Notifications/system', System_Notification.as_view(), name='system-notifications'),
-    path('Notifications/get_all', Get_All_Notification.as_view(), name='all-notifications'),
-    path('Notifications/read_one', Read_Notification.as_view(), name='read-notification'),
-    path('Notifications/read_all', Read_All_Notification.as_view(), name='read-all-notifications'),
-    path('Notifications/delete', Delete_Nnotification.as_view(), name='delete-notification'),
+    path(
+        "Notifications/unread",
+        UnRead_Notification.as_view(),
+        name="unread-notifications",
+    ),
+    path(
+        "Notifications/system",
+        System_Notification.as_view(),
+        name="system-notifications",
+    ),
+    path(
+        "Notifications/get_all",
+        Get_All_Notification.as_view(),
+        name="all-notifications",
+    ),
+    path(
+        "Notifications/read_one", Read_Notification.as_view(), name="read-notification"
+    ),
+    path(
+        "Notifications/read_all",
+        Read_All_Notification.as_view(),
+        name="read-all-notifications",
+    ),
+    path(
+        "Notifications/delete",
+        Delete_Nnotification.as_view(),
+        name="delete-notification",
+    ),
 ]
